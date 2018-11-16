3b811073b6c829abb43ed16c1d6a55f0c22036f9
Dimitar Ilkov <chazzbg@gmail.com>
2018-11-16 21:09:56 +0200

    add ide helper

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
